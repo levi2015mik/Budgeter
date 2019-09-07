13f9d6a34b7204dc8a4346bf1d0e03a260cf4008
Michael <lewiczkij.m@yandex.ru>
2019-09-07 17:13:47 +0300

    3--Start work of Home entry: list of planned purchases

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from './components/Header'
-import Home from './components/Home'
+import Home from './components/home/Home'
 import Categories from "./components/Categories";
 import List from "./components/List";
 import Help from "./components/Help";
 import { HashRouter, Route} from "react-router-dom";
+import css from './app.module.css';
 
 function App() {
 
     return (
         <div className="App">
             <HashRouter>
-                <Header/>
+                <Header className={css.header}/>
                 <Route exact path="/" render={Home}/>
                 <Route path="/categories" render={Categories}/>
                 <Route path="/list" render={List}/>
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home(props) {
+    return(
+        <div className={"n"}>
+            <input placeholder="Name of entry"/>
+            <input value="Add new entry" type="button"/>
+        </div>
+    )
+}
+
+export default Home
